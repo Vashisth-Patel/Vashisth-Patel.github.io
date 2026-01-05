63f1a08f2887a3dd77d3789ce5f228f2b59223c0
Patel Vashisth <patelvashisth842124@gmail.com>
2026-01-05 08:13:22 +0100

    fix: css added by updating layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // SITE must be your full GitHub URL
   site: 'https://Vashisth-Patel.github.io',
-  // BASE must be your repository name with leading and trailing slashes
   base: '/Vashisth-Patel.github.io',
   integrations: [tailwind()],
+  build: {
+    // This ensures assets are easy for GitHub to find
+    assets: '_assets'
+  },
+  // This helps with path resolution on subfolders
+  trailingSlash: 'always'
 });
